eee2d764eabbd5c5449581d116558e1685de1b65
haagmin5382 <itworker5382@gmail.com>
2022-05-07 12:23:23 +0900

    feat : add logout

--- redux-Toolkit/src/App.js
+++ redux-Toolkit/src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Profile from "./Profile";
 import Login from "./Login";
+import Logout from "./Logout";
 function App() {
   return (
     <div>
       <Profile />
       <Login />
+      <Logout />
     </div>
   );
 }
